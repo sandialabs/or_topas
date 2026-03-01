66ab3388ff9cda297dd0846f1d9141a4d65cac95
Matthew Viens <75225878+viens-code@users.noreply.github.com>
2026-03-01 15:14:22 -0600

    Commenting update in benders_serial

--- or_topas/benders/benders_serial.py
+++ or_topas/benders/benders_serial.py
@@ -228,12 +228,11 @@ class Benders_Serial(Benders_Abstract):
         return cuts_added
 
     def generate_cut_standard_lp_transform(self):
-        # print("Yes, using standard lp cut generation")
         coefficients = np.zeros(
             self.global_num_subproblems() * len(self.root_vars), dtype="d"
         )
         constants = np.zeros(self.global_num_subproblems(), dtype="d")
-        eta_gaps = np.zeros(self.global_num_subproblems(), dtype="d")
+        subproblem_etas = np.zeros(self.global_num_subproblems(), dtype="d")
 
         for local_subproblem_ndx in range(len(self.subproblems)):
             subproblem = self.subproblems[local_subproblem_ndx]
@@ -302,59 +301,25 @@ class Benders_Serial(Benders_Abstract):
             # so coefficients[coeff_ndx] = fix_cons.dual[root_var_to_fix_con[rv]], where coeff_ndx = global_subproblem_ndx * len(self.root_vars) + (i for i, v in enumerate(root_var_to_fix_con.keys() if v == rv)
 
             # TODO: we could just sub this out for rv, con in var_to_con_map.items()
-            constant_2 = 0
-            for root_var in self.root_vars:
-                if root_var in complicating_vars_map:
-                    c = var_to_con_map[root_var]
-                    coefficients[coeff_ndx] = sign_convention * pyo.value(
-                        subproblem.dual[c]
-                    )
-                    constant_2 += (
-                        sign_convention
-                        * pyo.value(subproblem.dual[c])
-                        * pyo.value(root_var)
-                    )
+
+            for root_var, c in var_to_con_map.items():
+                coefficients[coeff_ndx] = sign_convention * pyo.value(
+                    subproblem.dual[c]
+                )
                 coeff_ndx += 1
 
             # the constants are come from the sum of everything else for the subproblem as:
             # constants[global_subproblem_ndx] = sum(aux_con.dual[c]*pyo.value(aux_con_rhs[i]) for i,c in enumerate(aux_con))
             # all terms need to include the subproblem solver sign convention
-            # print(subproblem.dual.display())
-            # print(f"{subproblem.aux_cons=}, {len(subproblem.aux_cons)=}")
-            # print(subproblem.aux_cons.display())
-            # print(f"{str(subproblem.aux_cons[1])=}")
-            # subproblem.aux_cons[1].pprint()
-            # print(f"{str(subproblem.aux_cons_rhs_exprs[1])=}")
-
-            # constant_1 = -sign_convention * sum(
-            #     subproblem.dual[subproblem.aux_cons[c]]
-            #     * pyo.value(subproblem.aux_cons_rhs_exprs[i])
-            #     for i, c in enumerate(subproblem.aux_cons)
-            # )
-
-            # constant_2 = 0
-
-            # constants[global_subproblem_ndx] = constant_1 + constant_2
 
             constants[global_subproblem_ndx] = -sign_convention * sum(
                 subproblem.dual[subproblem.aux_cons[c]]
                 * pyo.value(subproblem.aux_cons_rhs_exprs[i])
                 for i, c in enumerate(subproblem.aux_cons)
             )
-            # print(f"{constants[global_subproblem_ndx]=}")
-            # constant = 0
-            # for index, cons in enumerate(subproblem.aux_cons):
-            #     print(f"{type(subproblem.dual[subproblem.aux_cons[cons]])=}, {type(index)=}")
-            #     constant += subproblem.dual[cons]*pyo.value(subproblem.aux_cons_rhs_exprs[index])
-            # constants[global_subproblem_ndx] = sign_convention*constant
 
-            # need the gap between root_eta and local_eta (subproblem obj value)
-            # this is the 'is a cut needed' test value
-            # print(f"{pyo.value(root_eta)=}")
-            # print(f"{pyo.value(subproblem.orig_obj_expr)=}")
-            eta_gaps[global_subproblem_ndx] = abs(
-                pyo.value(root_eta) - pyo.value(subproblem.orig_obj_expr)
-            )
+            # eta term directly comes from subproblem objective
+            subproblem_etas[global_subproblem_ndx] = pyo.value(subproblem.orig_obj_expr)
 
             # remove the added fixing constraints for fix_complicating vars
             if isinstance(subproblem_solver, PersistentSolver):
@@ -368,22 +333,25 @@ class Benders_Serial(Benders_Abstract):
         total_num_subproblems = self.global_num_subproblems()
         # global_constants = np.zeros(total_num_subproblems, dtype="d")
         # global_coeffs = np.zeros(total_num_subproblems * len(self.root_vars), dtype="d")
-        # global_eta_gaps = np.zeros(total_num_subproblems, dtype="d")
+        # global_subproblem_etas = np.zeros(total_num_subproblems, dtype="d")
 
         # comm = self.comm
         # comm.Allreduce([constants, MPI.DOUBLE], [global_constants, MPI.DOUBLE])
         # comm.Allreduce([coefficients, MPI.DOUBLE], [global_coeffs, MPI.DOUBLE])
-        # comm.Allreduce([eta_gaps, MPI.DOUBLE], [global_eta_gaps, MPI.DOUBLE])
+        # comm.Allreduce([subproblem_etas, MPI.DOUBLE], [global_subproblem_etas, MPI.DOUBLE])
 
         global_constants = constants
         global_coeffs = coefficients
-        global_eta_gaps = eta_gaps
-        # global_eta_coeffs = eta_coeffs
+        global_subproblem_etas = subproblem_etas
 
         global_constants = [float(i) for i in global_constants]
         global_coeffs = [float(i) for i in global_coeffs]
-        global_eta_gaps = [float(i) for i in global_eta_gaps]
-        # global_eta_coeffs = [float(i) for i in global_eta_coeffs]
+        global_subproblem_etas = [float(i) for i in global_subproblem_etas]
+
+        global_eta_gaps = [
+            abs(pyo.value(self.root_etas[i]) - v)
+            for i, v in enumerate(global_subproblem_etas)
+        ]
 
         # TODO: why do we need all threads computing the same cuts?
         # shouldn't this be blocked behind a rank check?
@@ -400,8 +368,13 @@ class Benders_Serial(Benders_Abstract):
                 for root_var in self.root_vars:
                     coeff = global_coeffs[coeff_ndx]
                     # this enforces a signe assumption
-                    # cut_expr -= coeff * (root_var - root_var.value)
+
+                    # transform case 1 for cut building, see transform details in Benders_Abstract
                     cut_expr -= coeff * root_var
+
+                    # transform case 2 for cut building
+                    # cut_expr -= coeff * (root_var - root_var.value)
+
                     coeff_ndx += 1
                 new_cut = self.cuts.add(cut_expr <= root_eta)
                 # print(f"Cut expr at present point: {pyo.value(cut_expr)}")
